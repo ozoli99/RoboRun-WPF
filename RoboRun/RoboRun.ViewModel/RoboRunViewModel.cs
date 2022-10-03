be60c10fe926a5c4c09b2e285ccfd2525dc5e296
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-03 22:21:24 +0200

    Add constructor and game event handlers to ViewModel

--- RoboRun/RoboRun.ViewModel/RoboRunViewModel.cs
+++ RoboRun/RoboRun.ViewModel/RoboRunViewModel.cs
@@ -12,5 +12,50 @@ namespace RoboRun.ViewModel
         private RoboRunModel _model;    // model
 
         #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Constructor
+
+        public RoboRunViewModel(RoboRunModel model)
+        {
+            _model = model;
+            _model.GameTimeAdvanced += new EventHandler<RoboRunEventArgs>(Model_GameTimeAdvanced);
+            _model.GameTimePaused += new EventHandler<RoboRunEventArgs>(Model_GameTimePaused);
+            _model.GameWin += new EventHandler<RoboRunEventArgs>(Model_GameWin);
+            _model.RobotMoved += new EventHandler(Model_RobotMoved);
+        }
+
+        #endregion
+
+        #region Game event handlers
+
+        private void Model_GameTimeAdvanced(object? sender, RoboRunEventArgs e)
+        {
+
+        }
+
+        private void Model_GameTimePaused(object? sender, RoboRunEventArgs e)
+        {
+
+        }
+
+        private void Model_GameWin(object? sender, RoboRunEventArgs e)
+        {
+
+        }
+
+        private void Model_RobotMoved(object? sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
     }
 }
