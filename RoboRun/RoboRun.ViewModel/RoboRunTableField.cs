c92944e332e004eb329a3e69d279e2e3393cc36e
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-04 00:09:50 +0200

    Create RoboRunTableField class

---
+++ RoboRun/RoboRun.ViewModel/RoboRunTableField.cs
@@ -0,0 +1,19 @@
+﻿namespace RoboRun.ViewModel
+{
+    public class RoboRunTableField : ViewModelBase
+    {
+        #region Private fields
+
+        #endregion
+
+        #region Properties
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public DelegateCommand StepCommand { get; set; }
+
+        #endregion
+    }
+}
