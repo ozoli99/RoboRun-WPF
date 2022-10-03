63b0c72ae23ca50b0d17e423323922c3fc4155bd
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-03 22:12:49 +0200

    Add RoboRun.ViewModel project and implement ViewModelBase abstract class

---
+++ RoboRun/RoboRun.ViewModel/ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RoboRun.ViewModel
+{
+    /// <summary>
+    /// Type of ViewModel base class.
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Events
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Instantiate ViewModel base class.
+        /// </summary>
+        protected ViewModelBase() { }
+
+        #endregion
+
+        #region Event methods
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
