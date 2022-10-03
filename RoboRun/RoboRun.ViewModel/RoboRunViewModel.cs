7264a9448d5f875bd9bdcac92bffeaa26bbe1e01
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-03 22:16:43 +0200

    Create RoboRunViewModel and add reference to RoboRun.Model

---
+++ RoboRun/RoboRun.ViewModel/RoboRunViewModel.cs
@@ -0,0 +1,16 @@
+﻿using RoboRun.Model;
+
+namespace RoboRun.ViewModel
+{
+    /// <summary>
+    /// Type of RoboRun ViewModel.
+    /// </summary>
+    public class RoboRunViewModel : ViewModelBase
+    {
+        #region Private fields
+
+        private RoboRunModel _model;    // model
+
+        #endregion
+    }
+}
