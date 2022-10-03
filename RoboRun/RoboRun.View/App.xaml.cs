a52f11ad40ace58f145d1ceb402446621e7addbd
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-03 22:50:46 +0200

    Add private fields to App class and implement constructor

--- RoboRun/RoboRun.View/App.xaml.cs
+++ RoboRun/RoboRun.View/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using RoboRun.Model;
+using RoboRun.Persistence;
+using RoboRun.ViewModel;
+using System;
 using System.Windows;
 
 namespace RoboRun.View
@@ -13,5 +11,42 @@ namespace RoboRun.View
     /// </summary>
     public partial class App : Application
     {
+        #region Private fields
+
+        private RoboRunModel _model;
+        private RoboRunViewModel _viewModel;
+        private MainWindow _view;
+
+        #endregion
+
+        #region Constructor
+
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+
+        #endregion
+
+        #region Application event handlers
+
+        private void App_Startup(object? sender, StartupEventArgs e)
+        {
+            _model = new RoboRunModel(new RoboRunFileDataAccess());
+            _model.GameWin += new EventHandler<RoboRunEventArgs>(Model_GameWin);            
+
+            _viewModel = new RoboRunViewModel(_model);
+            _viewModel.NewGame += new EventHandler(ViewModel_NewGame);
+            _viewModel.LoadGame += new EventHandler(ViewModel_LoadGame);
+            _viewModel.SaveGame += new EventHandler(ViewModel_SaveGame);
+            _viewModel.ExitGame += new EventHandler(ViewModel_ExitGame);
+
+            _view = new MainWindow();
+            _view.DataContext = _viewModel;
+            _view.Closing += new System.ComponentModel.CancelEventHandler(View_Closing);
+            _view.Show();
+        }
+
+        #endregion
     }
 }
