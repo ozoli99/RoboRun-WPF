254e7e69461bdd1823b257f7217235f6d28766c3
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-04 22:33:23 +0200

    Add private fields to RoboRunTableField class and fix properties in it

--- RoboRun/RoboRun.ViewModel/RoboRunTableField.cs
+++ RoboRun/RoboRun.ViewModel/RoboRunTableField.cs
@@ -4,6 +4,12 @@
     {
         #region Private fields
 
+        private bool _hasWall;
+        private bool _hasCollapsedWall;
+        private bool _isRobot;
+        private bool _isHome;
+        private bool _isFloor;
+
         #endregion
 
         #region Properties
@@ -12,15 +18,70 @@
 
         public int Y { get; set; }
 
-        public bool HasWall { get; set; }
+        public bool HasWall
+        { 
+            get { return _hasWall; }
+            set
+            {
+                if (_hasWall != value)
+                {
+                    _hasWall = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public bool HasCollapsedWall { get; set; }
+        public bool HasCollapsedWall
+        { 
+            get { return _hasCollapsedWall; }
+            set
+            {
+                if (_hasCollapsedWall != value)
+                {
+                    _hasCollapsedWall = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public bool IsRobot { get; set; }
+        public bool IsRobot
+        { 
+            get { return _isRobot; } 
+            set
+            {
+                if (_isRobot != value)
+                {
+                    _isRobot = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public bool IsHome { get; set; }
+        public bool IsHome
+        {
+            get { return _isHome; }
+            set
+            {
+                if (_isHome != value)
+                {
+                    _isHome = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public bool IsFloor { get; set; }
+        public bool IsFloor
+        {
+            get { return _isFloor; }
+            set
+            {
+                if (_isFloor != value)
+                {
+                    _isFloor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public DelegateCommand StepCommand { get; set; }
 
